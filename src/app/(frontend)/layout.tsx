c7df908e7786a150cc79ea03680d8a55a26e6e1f
dDein <dropsy.dice@gmail.com>
2026-03-02 19:48:42 +0100

    removed tailwind config

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -4,42 +4,24 @@ import './styles.css'
 import { cn } from '@/lib/utils'
 import { Inter as FontSans } from 'next/font/google'
 import { CartProvider } from './context/CartContext'
-import { Geologica } from "next/font/google";
-
-const geologica = Geologica({
-  subsets: ["latin"],
-  variable: "--font-geologica",
-  weight: ["200","300","400"],
-});
+import { Geologica } from 'next/font/google'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
 }
 
-const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
+const geologica = Geologica({
+  weight: ['300', '400'],
+  subsets: ['latin', 'cyrillic'],
 })
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Geologica:wght,CRSV@100..900,0&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className={cn('min-h-screen bg-background antialiased', geologica.variable)}>
+    <html lang="en" className={geologica.className}>
+      <body className={cn('min-h-screen bg-background antialiased')}>
         <CartProvider>
           <main>{children}</main>
         </CartProvider>
--- tailwind.config.js
+++
@@ -1,5 +0,0 @@
-extend: {
-    fontFamily: {
-      sans: ["var(--font-geologica)", "sans-serif"],
-    },
-  }
